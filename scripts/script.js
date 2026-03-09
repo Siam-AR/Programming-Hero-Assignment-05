e9650ba1f373c7a320dd8e9925352ced2e429146
Siam-AR <siam.ar.nexus@gmail.com>
2026-03-09 19:54:46 +0600

    fix(dashboard): recover deleted login-functionality & page-navigation-toggle

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,39 @@
+// Login functionality
+const usernameInput = document.querySelector('input[type="text"]');
+const passwordInput = document.querySelector('input[type="password"]');
+const loginButton = document.querySelector("button");
+
+const demoUserName = "admin";
+const demoPassword = "admin123";
+
+loginButton.addEventListener("click", () => {
+  const username = usernameInput.value.trim();
+  const password = passwordInput.value.trim();
+
+  if (username === demoUserName && password === demoPassword) {
+    alert("Login successful!");
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Invalid username or password.");
+  }
+});
+
+// Page Navigation Toggle
+const allFilterBtn = document.getElementById("all-filter-btn");
+const openFilterBtn = document.getElementById("open-filter-btn");
+const closedFilterBtn = document.getElementById("closed-filter-btn");
+function toggleStyle(id) {
+  allFilterBtn.classList.remove("btn-primary", "text-white");
+  openFilterBtn.classList.remove("btn-primary", "text-white");
+  closedFilterBtn.classList.remove("btn-primary", "text-white");
+
+  const selected = document.getElementById(id);
+  selected.classList.add("btn-primary", "text-white");
+}
+
 // Load All Issues
 const loadIssues = () => {
-  manageSpinner(true);
+  manageloadingSpiner(true);
 
   fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
     .then((res) => res.json())
@@ -137,7 +170,7 @@ const displayIssues = (issues) => {
     container.append(card);
   });
 
-  manageSpinner(false);
+  manageloadingSpiner(false);
 };
 
 // Load Issue Details
@@ -267,3 +300,4 @@ const displayIssueDetails = (issue) => {
 
   document.getElementById("issue_modal").showModal();
 };
+
